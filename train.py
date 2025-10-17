06406747554148ff5de05005c05dc605882e025d
hbkim-fobecon <hbkim@fobecon-ai.com>
2025-10-17 16:34:21 +0900

    tmp

--- train.py
+++ train.py
@@ -6,4 +6,4 @@ model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data="posco_under_windoor_winsldr_onlyinst_w640i128_251016.yaml", epochs=100, imgsz=640, batch=16)
+results = model.train(data="posco_under_windoor_winsldr_onlyinst_w640i128_251016.yaml", epochs=100, imgsz=960, batch=16)
